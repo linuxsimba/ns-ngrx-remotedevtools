15f8f4d9ffb2461f562e150b415565bdf4cd80b0
Stanley Karunditu <stanley@pebblecreeksystems.com>
2021-05-07 22:35:01 -0400

    add scss and remove hello world example. just add a button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
-import { NativeScriptModule } from '@nativescript/angular'
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule } from '@nativescript/angular';
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { ItemsComponent } from './item/items.component'
-import { ItemDetailComponent } from './item/item-detail.component'
+import { AppComponent } from './app.component';
+
 
 @NgModule({
-  bootstrap: [AppComponent],
-  imports: [NativeScriptModule, AppRoutingModule],
-  declarations: [AppComponent, ItemsComponent, ItemDetailComponent],
+  bootstrap: [ AppComponent ],
+  imports: [ NativeScriptModule ],
+  declarations: [ AppComponent ],
   providers: [],
-  schemas: [NO_ERRORS_SCHEMA],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule {}
